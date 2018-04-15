c6641f2b3beecf1cc837834d9ef2b3fde88e3944
René Fritz <r.fritz@colorcube.de>
2018-04-15 19:09:27 +0200

    misc cleanups in readme and extension config

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# Bookmark Pages
+# Bookmark Pages TYPO3 Extension
 
 This is a TYPO3 extension that provides bookmarks functionality of local pages for logged in frontend users.
+
+## Usage
 
 TypoScript and fluid templates are just examples. Adapt it to your needs.
 
 In the example templates there's nothing shown if the user is not logged in.
+
+Further information: https://docs.typo3.org/typo3cms/extensions/bookmark_pages/
+
+### Dependencies
+
+* TYPO3 6.2 - 8.7
+
+### Installation
+
+#### Installation using Composer
+
+In your Composer based TYPO3 project root, just do `composer require colorcube/bookmark-pages`. 
+
+#### Installation as extension from TYPO3 Extension Repository (TER)
+
+Download and install the extension with the extension manager module.
+
+## Remarks
+
+The magic happens on every page reload!
+
+## Contribute
+
+- Send pull requests to the repository. <https://github.com/colorcube/bookmark_pages>
+- Use the issue tracker for feedback and discussions. <https://github.com/colorcube/bookmark_pages/issues>
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,34 +1,32 @@
 <?php
 
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Bookmark Pages',
     'description' => 'Provides bookmarks functionality of local pages for logged in frontend users.',
     'category' => 'Plugins',
     'author' => 'René Fritz',
     'author_email' => 'r.fritz@colorcube.de',
     'author_company' => 'Colorcube',
-    'shy' => '',
-    'priority' => '',
-    'module' => '',
+    'version' => '1.2.0',
     'state' => 'stable',
-    'internal' => '',
-    'uploadfolder' => '0',
+    'uploadfolder' => 0,
     'createDirs' => '',
     'modify_tables' => '',
     'clearCacheOnLoad' => 1,
-    'lockType' => '',
-    'version' => '1.1.0',
-    'constraints' => array(
-        'depends' => array(
-            'php' => '5.3.7-7.0.999',
-            'typo3' => '6.2.0-7.6.999',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.0-8.7.999',
             'typoscript_rendering' => '*',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
-            'news' => '*',
-        ),
-    ),
-);
+        ],
+        'conflicts' => [],
+        'suggests' => [
+            'news' => '*'
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Colorcube\\BookmarkPages\\' => 'Classes'
+        ]
+    ]
+];

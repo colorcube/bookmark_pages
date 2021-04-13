59574e0e3900498cc5471b7914d7e2e3a52ea188
roman <rb@buechler.pro>
2021-04-13 18:53:11 +0200

    [TASK] Update plugin configuration/registration

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,16 +1,29 @@
 <?php
 defined('TYPO3') || die('Access denied.');
 
-/**
- * Register Plugin
- */
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'bookmark_pages',
-    'Bookmarks',
-    'Bookmark Pages'
-);
+(function () {
+    $version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::getCurrentTypo3Version();
+    $version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger($version);
+    /**
+     * Register Plugin
+     */
+    if ($version < 10000000) {
+        // For TYPO3 < V10
+        // @extensionScannerIgnoreLine
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            'Colorcube.BookmarkPages',
+            'Bookmarks',
+            'Bookmark Pages'
+        );
+    } else {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            'BookmarkPages',
+            'Bookmarks',
+            'Bookmark Pages'
+        );
+    }
 
-$pluginSignature = 'bookmarkpages_bookmarks';
+    $pluginSignature = 'bookmarkpages_bookmarks';
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'recursive,select_key,pages';
-
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'recursive,select_key,pages';
+})();
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,15 +1,33 @@
 <?php
 defined('TYPO3') or die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Colorcube.' . $_EXTKEY,
-    'Bookmarks',
-    array(
-        'Bookmarks' => 'index, bookmark, delete',
-    ),
-    // non-cacheable actions
-    array(
-        'Bookmarks' => 'index, bookmark, delete',
-    )
-);
-
+(function () {
+    $version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::getCurrentTypo3Version();
+    $version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger($version);
+    if ($version < 10000000) {
+        // For TYPO3 < V10
+        // @extensionScannerIgnoreLine
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'Colorcube.BookmarkPages',
+            'Bookmarks',
+            array(
+                'Bookmarks' => 'index, bookmark, delete',
+            ),
+            // non-cacheable actions
+            array(
+                'Bookmarks' => 'index, bookmark, delete',
+            )
+        );
+    } else {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'BookmarkPages',
+            'Bookmarks',
+            [
+                \Colorcube\BookmarkPages\Controller\BookmarksController::class => 'index, bookmark, delete'
+            ],
+            [
+                \Colorcube\BookmarkPages\Controller\BookmarksController::class => 'index, bookmark, delete'
+            ]
+        );
+    }
+})();
